5c1ca38702bbe3fed7373c146770414af0050be9
patrick.lafrance <patrick.lafrance@outlook.com>
2020-01-13 09:54:43 -0500

    Removed dependency to SVGR

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,1 +1,7 @@
-export { ReactComponent as CheckmarkIcon } from "./icon-checkmark.svg";
+export function CheckmarkIcon(props) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" {...props}>
+            <path d="M4.2 8.4l-3-3c-.3-.3-.7-.3-.9 0-.3.3-.3.7 0 .9l4 4 7.6-7.6c.3-.3.3-.7 0-.9-.3-.3-.7-.3-.9 0L4.2 8.4z" />
+        </svg>
+    );
+}
